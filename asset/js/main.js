adf2977c09dee35589e174f58fb2d2fcf17d90ba
Ga <gabri.magni.97@gmail.com>
2022-04-01 16:44:17 +0200

    add & remove

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,14 +4,15 @@
 - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
 MILESTONE 1
-Stampare all'interno di una lista, un item per ogni todo.
-Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+2) Stampare all'interno di una lista, un item per ogni todo.
+
+3) Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
 MILESTONE 2
-Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+4) Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
 
 MILESTONE 3
-Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+5) Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
 2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
@@ -23,20 +24,31 @@ var app = new Vue(
         data: {
             todos: [
                 {
-                    text: "",
+                    text: "lista della spesa",
                     done: false
                 },
                 {
-                    text: "",
-                    done: false
+                    text: "fare benzina",
+                    done: true
                 },
                 {
-                    text: "",
+                    text: "sistemare la bici",
                     done: false
                 },
             ],
+            newTodo: "",
         },
         methods: {
+            aggiungiTodo: function() {
+                let newTodoObj = {
+                    text: this.newTodo,
+                    done: false
+                };
+                this.todos.push(newTodoObj);
+            },
+            rimuoviTodo: function(todoIndex) {  
+                this.todos.splice(todoIndex, 1);
+            }
         }
     }
 );
